ba6d0f28024b12a4fdfb64a5f775e91fb8532673
Riccardo Torello <rikitorello2001@gmail.com>
2018-12-18 17:37:00 +0100

    ho creato un flow che permette di convertire il numero 200 in base binario

---
+++ GITHUB/GitHub/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //inizializzo una variabile ad intero
+            int numero = 200;
+            string conversione = Convert.ToString(numero,2);
+            //stampo il risultato
+            Console.WriteLine($"il numero {numero} in base binaria divanta {conversione}");
+            Console.ReadLine();
+        }
+    }
+}
