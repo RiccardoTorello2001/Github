42b6d944e48805390db81dd17f24e1215c0e57a7
Riccardo Torello <rikitorello2001@gmail.com>
2018-12-18 17:39:07 +0100

    ho chietso all'utenre di scrivere in input un numero da convertire

---
+++ GitHub/GitHub/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //inizializzo una variabile ad intero
+            int numero = 0;
+            Console.WriteLine("inserisci un numero da convertire");
+            numero = Convert.ToInt32(Console.ReadLine());
+            string conversione = Convert.ToString(numero,2);
+            //stampo il risultato
+            Console.WriteLine($"il numero {numero} in base binaria divanta {conversione}");
+            Console.ReadLine();
+        }
+    }
+}
