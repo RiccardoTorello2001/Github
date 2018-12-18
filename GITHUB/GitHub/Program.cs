e6f2d4964de85021fdc2cee706bceaa4a270f77f
Riccardo Torello <rikitorello2001@gmail.com>
2018-12-18 17:42:26 +0100

    ho chiesto all'utente di inserire in input la base per far convertire il  numero e ho modificato l'output in modo da renderlo più efficace

--- GITHUB/GitHub/Program.cs
+++ GITHUB/GitHub/Program.cs
@@ -11,10 +11,14 @@ namespace GitHub
         static void Main(string[] args)
         {
             //inizializzo una variabile ad intero
-            int numero = 200;
-            string conversione = Convert.ToString(numero,2);
+            int numero = 0, value = 0;
+            Console.WriteLine("inserisci un numero da convertire");
+            numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("inserisci la base per far convertire il numero");
+            value = Convert.ToInt32(Console.ReadLine());
+            string conversione = Convert.ToString(numero,value);
             //stampo il risultato
-            Console.WriteLine($"il numero {numero} in base binaria divanta {conversione}");
+            Console.WriteLine($"il numero {numero} in base {value} divanta {conversione}");
             Console.ReadLine();
         }
     }
